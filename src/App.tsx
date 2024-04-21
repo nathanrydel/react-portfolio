96744d7df9b1e112dbe5a39d7b7c333ef7ff8c3d
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 13:19:57 -0700

    Added in proposed  components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 export default function App() {
@@ -13,6 +15,9 @@ export default function App() {
         <Navbar />
         <Hero />
         <Technologies />
+        <Projects />
+        <About />
+        <Contact />
       </div>
     </div>
   );
