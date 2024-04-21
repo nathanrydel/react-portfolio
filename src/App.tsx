beb7c77397ff82efd24143e2177d8382eb23f639
Nathan Rydel <nathanrydel@gmail.com>
2024-04-20 22:40:26 -0700

    update background styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 size-full"></div>
-      <div className="absolute inset-0 -z-10 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-      </div>
-      <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900 w-full h-screen">
+      <div className="relative size-full bg-slate-950">
+        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
+        </div>
+        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div className="container mx-auto px-8">
+          <Navbar />
+          <Hero />
+        </div>
       </div>
     </div>
   );
