ea3ec3f1ea04f392d9e671ce516540f05b394c78
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 15:57:09 -0700

    move db.ts location

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -4,7 +4,7 @@ import warblerImage from "../assets/projects/WarblerDemo.webp";
 
 export const HERO_CONTENT = `I am a passionate Full Stack Developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, Express, Flask, and PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile Full Stack Developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile Full Stack Developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, PostgreSQL, Flask, and Express. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const PROJECTS = [
   {
@@ -14,7 +14,7 @@ export const PROJECTS = [
     description:
       "A fully functional job board with authentication, applications, and search features.",
     link: "https://rydel-jobly.surge.sh/",
-    technologies: ["React", "Node.js", "Express"],
+    technologies: ["React", "Node.js", "Bootstrap", "Express"],
   },
   {
     id: "friender",
