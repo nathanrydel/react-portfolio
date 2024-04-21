198121536131061c005fd710c56458d22b825eeb
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 11:11:47 -0700

    added link for main icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,12 @@ function Navbar() {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex shrink-0 items-center">
-        <span className="mx-2 text-3xl">
+        {/* TODO: Make an SVG Logo */}
+        <a className="mx-2 text-3xl" href="/">
           Nr
-        </span>
+        </a>
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-6 text-2xl">
         <a href="https://github.com/nathanrydel">
           <FaGithub />
         </a>
