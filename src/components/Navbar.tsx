df3644cfe9f7e77d3981471e33063ebb59f3b618
Nathan Rydel <nathanrydel@gmail.com>
2024-04-20 21:42:25 -0700

    updated styles for tailwind 3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,10 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 function Navbar() {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        NR
+      <div className="flex shrink-0 items-center">
+        <span className="text-4xl">
+          Nathan Rydel
+        </span>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
