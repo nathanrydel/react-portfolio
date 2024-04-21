dccc37d84b933607e6a87ee00cc93b64f4465610
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 11:11:21 -0700

    update bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900 size-full">
-      <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
-      </div>
-      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
