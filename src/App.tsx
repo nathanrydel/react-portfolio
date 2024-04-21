1871083326d1c94ac49b8651c15585209b56f989
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 10:55:01 -0700

    Update App styles
    - background not on full app
    - added base bg to body tag
    - update App to reflect changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900 w-full h-screen">
-      <div className="relative size-full bg-slate-950">
-        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
-        </div>
-        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        <div className="container mx-auto px-8">
-          <Navbar />
-          <Hero />
-        </div>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900 size-full">
+      <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
+      </div>
+      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
       </div>
     </div>
   );
