a084aaf104d78774230d122d4afb011a52aa09d9
Nathan Rydel <nathanrydel@gmail.com>
2024-04-20 22:17:48 -0700

    Hero component
    - created and styled Hero component
    - updated App to use component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       </div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
