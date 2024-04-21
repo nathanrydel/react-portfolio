d129add261d17b2ad4f925d76860478941e66232
Nathan Rydel <nathanrydel@gmail.com>
2024-04-21 13:19:40 -0700

    update db

--- src/assets/db.ts
+++ src/assets/db.ts
@@ -1,27 +1,37 @@
+import joblyImage from "../assets/projects/JoblyV2Demo.webp";
+import frienderImage from "../assets/projects/FrienderDemo.webp";
+import warblerImage from "../assets/projects/WarblerDemo.webp";
+
 export const HERO_CONTENT = `I am a passionate Full Stack Developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, Express, Flask, and PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile Full Stack Developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const PROJECTS = [
   {
+    id: "jobly",
     title: "Jobly V2",
-    image: "project1",
+    image: joblyImage,
     description:
       "A fully functional job board with authentication, applications, and search features.",
+    link: "https://rydel-jobly.surge.sh/",
     technologies: ["React", "Node.js", "Express"],
   },
   {
+    id: "friender",
     title: "Friender",
-    image: "project2",
+    image: frienderImage,
     description:
       "An application for finding people with similar hobbies and interests within a geographic area.",
+    link: "https://rydel-friender-demo.onrender.com/",
     technologies: ["HTML", "CSS", "Bootstrap", "Flask", "PostgreSQL"],
   },
   {
+    id: "warbler",
     title: "Warbler",
-    image: "project3",
+    image: warblerImage,
     description:
       "A full stack social networking site from a forgotten era.",
+    link: "https://rydel-warbler-demo.onrender.com/",
     technologies: ["HTML", "CSS", "Bootstrap", "Flask", "PostgreSQL"],
   },
 ];
