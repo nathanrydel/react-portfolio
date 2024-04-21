5613cfb32049e4b3a0536609df142c6293d62271
Nathan Rydel <nathanrydel@gmail.com>
2024-04-20 21:30:12 -0700

    Created Navbar comp
    - using TypeScript to create a Navbar
    - styling with TailwindCSS

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        NR
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
